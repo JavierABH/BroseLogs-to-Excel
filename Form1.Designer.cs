503dae6c65cc6f10e5ccdd34f4ad65c6686ab7bd
JavierABH <javierfchernandez10@gmail.com>
2022-10-12 07:42:12 -0500

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace Combinador_de_datos_Brose
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxPathFiles = new System.Windows.Forms.TextBox();
+            this.buttonSearchPath = new System.Windows.Forms.Button();
+            this.dateTimePickerFechaInicial = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dateTimePickerFechaFinal = new System.Windows.Forms.DateTimePicker();
+            this.buttonFolder = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.buttonCombinarDatos = new System.Windows.Forms.Button();
+            this.textBoxConsole = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxPathExcel = new System.Windows.Forms.TextBox();
+            this.buttonSaveExcel = new System.Windows.Forms.Button();
+            this.buttonIraExcel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ruta de los archivos";
+            // 
+            // textBoxPathFiles
+            // 
+            this.textBoxPathFiles.Location = new System.Drawing.Point(12, 25);
+            this.textBoxPathFiles.Name = "textBoxPathFiles";
+            this.textBoxPathFiles.Size = new System.Drawing.Size(415, 20);
+            this.textBoxPathFiles.TabIndex = 1;
+            // 
+            // buttonSearchPath
+            // 
+            this.buttonSearchPath.AutoSize = true;
+            this.buttonSearchPath.Location = new System.Drawing.Point(433, 25);
+            this.buttonSearchPath.Name = "buttonSearchPath";
+            this.buttonSearchPath.Size = new System.Drawing.Size(28, 23);
+            this.buttonSearchPath.TabIndex = 52;
+            this.buttonSearchPath.Text = "...";
+            this.buttonSearchPath.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.buttonSearchPath.UseVisualStyleBackColor = true;
+            this.buttonSearchPath.Click += new System.EventHandler(this.buttonSearchPath_Click);
+            // 
+            // dateTimePickerFechaInicial
+            // 
+            this.dateTimePickerFechaInicial.CustomFormat = "dd/MMM/yy";
+            this.dateTimePickerFechaInicial.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePickerFechaInicial.Location = new System.Drawing.Point(12, 64);
+            this.dateTimePickerFechaInicial.Name = "dateTimePickerFechaInicial";
+            this.dateTimePickerFechaInicial.Size = new System.Drawing.Size(158, 20);
+            this.dateTimePickerFechaInicial.TabIndex = 53;
+            this.dateTimePickerFechaInicial.ValueChanged += new System.EventHandler(this.dateTimePickerFechaInicial_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.TabIndex = 54;
+            this.label2.Text = "Fecha inicial";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(266, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 56;
+            this.label3.Text = "Fecha Final";
+            // 
+            // dateTimePickerFechaFinal
+            // 
+            this.dateTimePickerFechaFinal.CustomFormat = "MMM/dd/yyyy";
+            this.dateTimePickerFechaFinal.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePickerFechaFinal.Location = new System.Drawing.Point(269, 64);
+            this.dateTimePickerFechaFinal.Name = "dateTimePickerFechaFinal";
+            this.dateTimePickerFechaFinal.Size = new System.Drawing.Size(158, 20);
+            this.dateTimePickerFechaFinal.TabIndex = 55;
+            this.dateTimePickerFechaFinal.ValueChanged += new System.EventHandler(this.dateTimePickerFechaFinal_ValueChanged);
+            // 
+            // buttonFolder
+            // 
+            this.buttonFolder.Location = new System.Drawing.Point(12, 90);
+            this.buttonFolder.Name = "buttonFolder";
+            this.buttonFolder.Size = new System.Drawing.Size(127, 26);
+            this.buttonFolder.TabIndex = 57;
+            this.buttonFolder.Text = "Reordenar carpetas";
+            this.buttonFolder.UseVisualStyleBackColor = true;
+            this.buttonFolder.Click += new System.EventHandler(this.buttonFolder_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 108);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 58;
+            // 
+            // buttonCombinarDatos
+            // 
+            this.buttonCombinarDatos.Location = new System.Drawing.Point(12, 161);
+            this.buttonCombinarDatos.Name = "buttonCombinarDatos";
+            this.buttonCombinarDatos.Size = new System.Drawing.Size(126, 26);
+            this.buttonCombinarDatos.TabIndex = 59;
+            this.buttonCombinarDatos.Text = "Combinar datos";
+            this.buttonCombinarDatos.UseVisualStyleBackColor = true;
+            this.buttonCombinarDatos.Click += new System.EventHandler(this.buttonCombinarDatos_Click);
+            // 
+            // textBoxConsole
+            // 
+            this.textBoxConsole.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.textBoxConsole.Location = new System.Drawing.Point(12, 193);
+            this.textBoxConsole.Multiline = true;
+            this.textBoxConsole.Name = "textBoxConsole";
+            this.textBoxConsole.ReadOnly = true;
+            this.textBoxConsole.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxConsole.Size = new System.Drawing.Size(454, 209);
+            this.textBoxConsole.TabIndex = 61;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 119);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(161, 13);
+            this.label5.TabIndex = 62;
+            this.label5.Text = "Ruta donde se guardara el excel";
+            // 
+            // textBoxPathExcel
+            // 
+            this.textBoxPathExcel.Location = new System.Drawing.Point(12, 135);
+            this.textBoxPathExcel.Name = "textBoxPathExcel";
+            this.textBoxPathExcel.Size = new System.Drawing.Size(415, 20);
+            this.textBoxPathExcel.TabIndex = 63;
+            // 
+            // buttonSaveExcel
+            // 
+            this.buttonSaveExcel.AutoSize = true;
+            this.buttonSaveExcel.Location = new System.Drawing.Point(433, 135);
+            this.buttonSaveExcel.Name = "buttonSaveExcel";
+            this.buttonSaveExcel.Size = new System.Drawing.Size(26, 23);
+            this.buttonSaveExcel.TabIndex = 64;
+            this.buttonSaveExcel.Text = "...";
+            this.buttonSaveExcel.UseVisualStyleBackColor = true;
+            this.buttonSaveExcel.Click += new System.EventHandler(this.buttonSaveExcel_Click);
+            // 
+            // buttonIraExcel
+            // 
+            this.buttonIraExcel.Location = new System.Drawing.Point(384, 164);
+            this.buttonIraExcel.Name = "buttonIraExcel";
+            this.buttonIraExcel.Size = new System.Drawing.Size(75, 26);
+            this.buttonIraExcel.TabIndex = 65;
+            this.buttonIraExcel.Text = "Ir a Excel";
+            this.buttonIraExcel.UseVisualStyleBackColor = true;
+            this.buttonIraExcel.Click += new System.EventHandler(this.buttonIraExcel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(473, 414);
+            this.Controls.Add(this.buttonIraExcel);
+            this.Controls.Add(this.buttonSaveExcel);
+            this.Controls.Add(this.textBoxPathExcel);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxConsole);
+            this.Controls.Add(this.buttonCombinarDatos);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.buttonFolder);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dateTimePickerFechaFinal);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dateTimePickerFechaInicial);
+            this.Controls.Add(this.textBoxPathFiles);
+            this.Controls.Add(this.buttonSearchPath);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Brose logs to Excel";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxPathFiles;
+        private System.Windows.Forms.Button buttonSearchPath;
+        private System.Windows.Forms.DateTimePicker dateTimePickerFechaInicial;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dateTimePickerFechaFinal;
+        private System.Windows.Forms.Button buttonFolder;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button buttonCombinarDatos;
+        private System.Windows.Forms.TextBox textBoxConsole;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxPathExcel;
+        private System.Windows.Forms.Button buttonSaveExcel;
+        private System.Windows.Forms.Button buttonIraExcel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace Combinador_de_datos_Brose
+{
+    public partial class Form1 : Form
+    {
+        string path = "";
+        string pathExcel = "";
+        DateTime FechaInicio = DateTime.Today;
+        DateTime FechaFinal = DateTime.Today;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSearchPath_Click(object sender, EventArgs e)
+        {
+            // Seleccionando la ruta
+            FolderBrowserDialog ExplorerPathSelect = new FolderBrowserDialog();
+            if (ExplorerPathSelect.ShowDialog() == DialogResult.OK)
+            {
+                textBoxPathFiles.Text = ExplorerPathSelect.SelectedPath;
+                path = textBoxPathFiles.Text;
+            }    
+        }
+
+        private void dateTimePickerFechaInicial_ValueChanged(object sender, EventArgs e)
+        {
+            FechaInicio = dateTimePickerFechaInicial.Value;
+        }
+
+        private void dateTimePickerFechaFinal_ValueChanged(object sender, EventArgs e)
+        {
+            FechaFinal = dateTimePickerFechaFinal.Value;
+        }
+
+        private void buttonFolder_Click(object sender, EventArgs e)
+        {
+            path = textBoxPathFiles.Text;
+            string pathFile;
+            string pathFileRename;
+            if (textBoxPathFiles.Text == "")
+                MessageBox.Show("No ha seleccionado ninguna ruta", "Seleccione una ruta", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            else
+            {
+                try
+                {
+                    textBoxConsole.AppendText("Proceso de renombre de carpetas");
+                    textBoxConsole.AppendText(Environment.NewLine);
+                    while (true)
+                    {
+                        if (FechaInicio > FechaFinal)
+                            break;
+                        // Se empiezan a leer las carpetas para realizar el cambio de nombre
+                        pathFile = path + "\\" + FechaInicio.ToString("dd-MMM-yy");
+                        pathFileRename = path + "\\" + FechaInicio.ToString("MM-dd-yy");
+                        try
+                        {
+                            Directory.Move(pathFile, pathFileRename);
+                            textBoxConsole.AppendText(pathFileRename);
+                            textBoxConsole.AppendText(Environment.NewLine);
+                            textBoxConsole.AppendText("Hecho");
+                            textBoxConsole.AppendText(Environment.NewLine);
+                            FechaInicio = FechaInicio.AddDays(1);
+                        }
+                        catch (Exception DateNotFound)
+                        {
+                            if (DateNotFound is DirectoryNotFoundException) // Si no encuentra la carpeta, sigue
+                            {
+                                FechaInicio = FechaInicio.AddDays(1);
+                                continue;   
+                            }
+                        }
+                    }
+                    textBoxConsole.AppendText("Proceso terminado");
+                    textBoxConsole.AppendText(Environment.NewLine);
+                    MessageBox.Show("Las carpetas se han renombreado", "Proceso completado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                FechaInicio = dateTimePickerFechaInicial.Value;
+                FechaFinal = dateTimePickerFechaFinal.Value;
+            }
+        }
+        private void buttonSaveExcel_Click(object sender, EventArgs e)
+        {
+            // Donde se guardara el archivo de excel
+            SaveFileDialog ExplorerSaveExcel = new SaveFileDialog();
+            ExplorerSaveExcel.Title = "Exportar excel...";
+            if (ExplorerSaveExcel.ShowDialog() == DialogResult.OK)
+            {
+                textBoxPathExcel.Text = ExplorerSaveExcel.FileName;
+                pathExcel = textBoxPathExcel.Text;
+            }
+        }
+
+        private void buttonCombinarDatos_Click(object sender, EventArgs e)
+        {
+            pathExcel = textBoxPathExcel.Text;
+            string pathExcelFormat = pathExcel + ".xlsx";
+            string PathCSV = AppDomain.CurrentDomain.BaseDirectory + "\\Template\\Datos.csv";
+            string PathCSVCopy = AppDomain.CurrentDomain.BaseDirectory + "\\Template\\DatosCopy.csv";
+            string FileAdd;
+            StreamReader Reader;
+            StreamWriter Writer;
+            string array;
+            string contenidodelimitado;
+            if (textBoxPathExcel.Text == "")
+                MessageBox.Show("No ha seleccionado ninguna ruta", "Seleccione una ruta", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            else
+            {
+                try
+                {
+                    try // Codigo que para tener una sola copia del template
+                    {
+                        File.Copy(PathCSV, PathCSVCopy);
+                    }
+                    catch (Exception CsvExist)
+                    {
+                        if (CsvExist is IOException)
+                        {
+                            File.Delete(PathCSVCopy);
+                            File.Copy(PathCSV, PathCSVCopy);
+                        } // Termina codigo
+                    }
+
+                    Writer = File.AppendText(PathCSVCopy);
+                    string[] dato = new string[290];
+                    while (true)
+                    {
+                        if (FechaInicio > FechaFinal)
+                            break;
+                        try
+                        {
+                            // Lectura de cada archivo
+                            FileAdd = path + "\\" + FechaInicio.ToString("MM-dd-yy") + @"\Ford P552 HVAC.dat"; // Ejemplo: 02-01-22\Ford P552 HVAC.dat
+                            Reader = File.OpenText(FileAdd);
+                            array = Reader.ReadLine();
+                            while (array != null)
+                            {
+                                dato = array.Split('	');
+                                contenidodelimitado = array.Replace('	', ',');
+                                if (dato[0].Trim() != "Model No.")
+                                {
+                                    Writer.WriteLine(contenidodelimitado); // Copia solo los datos. No encabezado
+                                }
+                                array = Reader.ReadLine();
+                            }
+                            Reader.Close();
+                            textBoxConsole.AppendText(FileAdd + " Add");
+                            textBoxConsole.AppendText(Environment.NewLine);
+                            FechaInicio = FechaInicio.AddDays(1);
+                        }
+                        catch (Exception DateNotFound)
+                        {
+                            if (DateNotFound is DirectoryNotFoundException) // Si no encuentra la carpeta, sigue
+                            {
+                                FechaInicio = FechaInicio.AddDays(1);
+                                continue;
+                            }
+                        }
+                    }
+                    Writer.Close();
+                    textBoxConsole.AppendText("Proceso completado...");
+                    textBoxConsole.AppendText(Environment.NewLine);
+                    textBoxConsole.AppendText("Iniciando proceso de conversion a excel...");
+                    textBoxConsole.AppendText(Environment.NewLine);
+                    CsvToExcel(PathCSVCopy, pathExcelFormat);
+                    textBoxConsole.AppendText("Excel generado correctamente...");
+                    textBoxConsole.AppendText(Environment.NewLine);
+                    File.Delete(PathCSVCopy);
+                    DialogResult Ir_a_Excel = MessageBox.Show("Desea ir a la ruta del archivo excel?", "Conversion a excel terminada...", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                    if (Ir_a_Excel == DialogResult.Yes)
+                    {
+                        string argument = "/select, \"" + pathExcel + "\"";
+                        System.Diagnostics.Process.Start("explorer.exe", argument + ".xlsx");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void buttonIraExcel_Click(object sender, EventArgs e)
+        {
+            pathExcel = textBoxPathExcel.Text;
+            try
+            {
+                string argument = "/select, \"" + pathExcel + "\"";
+                System.Diagnostics.Process.Start("explorer.exe", argument + ".xlsx");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        static void CsvToExcel(string csv, string xlsx)
+        {
+            Excel.Application xl = new Excel.Application();
+            //Open Excel Workbook for conversion.
+            Excel.Workbook wb = xl.Workbooks.Open(csv);
+            Excel.Worksheet ws = (Excel.Worksheet)wb.Worksheets.get_Item(1);
+            //Select The UsedRange
+            Excel.Range used = ws.UsedRange;
+            //Autofit The Columns
+            used.EntireColumn.AutoFit();
+            //Save file as csv file
+            wb.SaveAs(xlsx, 51);
+            //Close the Workbook.
+            wb.Close();
+            //Quit Excel Application.
+            xl.Quit();
+        }
+    }
+}
